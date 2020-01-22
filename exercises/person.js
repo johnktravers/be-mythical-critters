88a5966791739a50c239e4fe33fe1986a2a44723
John Travers <johnk.travers@gmail.com>
2020-01-22 08:49:53 -0700

    Create person class with name property and stone and unstone functions

---
+++ exercises/person.js
@@ -0,0 +1,16 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+    this.stoned = false;
+  }
+
+  stone() {
+    this.stoned = true;
+  }
+
+  unstone() {
+    this.stoned = false;
+  }
+}
+
+module.exports = Person;
